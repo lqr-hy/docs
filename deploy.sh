f4c8b7b714c906c7cc84126e3046744f8b0afa16
lqr <1278297890@qq.com>
2023-03-29 20:28:40 +0800

    feat: 脚本

--- deploy.sh
+++ deploy.sh
@@ -12,14 +12,14 @@ cd docs/.vuepress/dist
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
+#git init
+#git add -A
+#git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:lqrDream/docs.git master:gh-pages
+git push -f git@github.com:lqr-hy/docs.git master:gh-pages
 
 cd -
